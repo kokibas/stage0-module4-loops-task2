740ae45eeba7d105a74c326dee5cb22b92527b5e
kokibas <nurym0807@gmail.com>
2022-11-03 16:10:59 +0600

    just rere

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
+        if (printTillInclusive % 2 == 0){
+            while (printTillInclusive >= 0){
+                System.out.println(printTillInclusive);
+                printTillInclusive++;
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.loops.task2;
 
+import java.util.Scanner;
+
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+//        Scanner input = new Scanner(System.in);
+        while (multiplyByAndToInclusive >= 0 || multiplyByAndToInclusive <= 0){
+            System.out.println(multiplyByAndToInclusive);
+            multiplyByAndToInclusive++;
+
+
+        }
     }
 }
