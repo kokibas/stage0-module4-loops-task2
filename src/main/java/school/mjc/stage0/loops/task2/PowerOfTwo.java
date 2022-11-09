89deb10230634e17ce50f22ac617cb7ee878a26e
kokibas <nurym0807@gmail.com>
2022-11-09 17:36:10 +0600

    qw1

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,8 +3,8 @@ package school.mjc.stage0.loops.task2;
 public class PowerOfTwo {
     public void printPower(int power) {
         int i = 0;
-        if (power <= i) {
-            while (power <= i) {
+        if (power >= i) {
+            while (power >= i) {
                 power = power * i;
                 i++;
                 System.out.println(power);
